800fbc059fd066bee233f0883a13c04876844121
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-03-28 23:48:56 +0530

    t

--- backend/monitor/src/main/java/com/monitor/monitor/ApplicationMonitor.java
+++ backend/monitor/src/main/java/com/monitor/monitor/ApplicationMonitor.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationMonitor {
-	//start of the application
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationMonitor.class, args);
 	}
