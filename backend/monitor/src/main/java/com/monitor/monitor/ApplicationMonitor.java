db5313096e8a5c0cd5ede9ff8ee7e785ad01d85c
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-04-15 17:38:50 +0530

    simple check

--- backend/monitor/src/main/java/com/monitor/monitor/ApplicationMonitor.java
+++ backend/monitor/src/main/java/com/monitor/monitor/ApplicationMonitor.java
@@ -1,6 +1,7 @@
 package com.monitor.monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//15-4-2026
 @SpringBootApplication
 public class ApplicationMonitor {
 	public static void main(String[] args) {
